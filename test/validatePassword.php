d2e7ba97dc42b0a49a2eb36547cf233359213fe8
Mahavishnu <mahavishnu.ramakrishnan@aspiresys.com>
2018-07-05 16:43:13 -0400

    Jenkins test

---
+++ test/validatePassword.php
@@ -0,0 +1,8 @@
+<?php
+
+class validatePassword extends PHPUnit_Framework_TestCase {
+	public function testValidLength() {
+		$valPass = new ValidatePassword();
+		$this->assertFalse($valPass->validLength('1234'));	
+	}
+}
